17a075b56fe738087f74812b7a0db4742d2fe1ec
Zakaria Al-onyny <zkryaalhnyny0@gmail.com>
2026-04-30 14:36:36 +0300

    chore: remove large/temp files from commit

--- test_login.ps1
+++
@@ -1,8 +0,0 @@
-$body = @{
-    identifier = "applicant@mojaz.gov.sa"
-    password = "Password123!"
-    method = "Email"
-} | ConvertTo-Json
-
-$response = Invoke-RestMethod -Uri "http://localhost:5013/api/v1/auth/login" -Method Post -ContentType "application/json" -Body $body
-$response | ConvertTo-Json -Depth 5
